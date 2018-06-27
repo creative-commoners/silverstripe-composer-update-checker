950a6c9e5d1b75133c31648609771684d0e862d8
Robbie Averill <robbie@silverstripe.com>
2018-06-27 14:54:42 +1200

    FIX onAfterBuild only looks at COMPOSER_HOME environment variable

--- src/Extensions/ComposerLoaderExtension.php
+++ src/Extensions/ComposerLoaderExtension.php
@@ -99,8 +99,8 @@ class ComposerLoaderExtension extends Extension
      */
     public function onAfterBuild()
     {
-        // Mock COMPOSER_HOME if it's not defined already. Composer requires one of the two to be set.
-        if (!getenv('HOME') && !getenv('COMPOSER_HOME')) {
+        // Mock COMPOSER_HOME if it's not defined already
+        if (!getenv('COMPOSER_HOME')) {
             putenv('COMPOSER_HOME=/tmp');
         }
 
